a274fcde55754a73d9d51b6c8841ebb2dfe03683
remington <remigreenbauer@gmail.com>
2021-03-11 20:43:43 -0500

    jacob change

--- routes/twitter.js
+++ routes/twitter.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const twitter = express.Router();
 var Twitter = require('twitter');
+const needle = require('needle');
  
 var client = new Twitter({
     consumer_key: 'sWfwBmRZHg2u0CPThpo0JHY0j',
     consumer_secret: 'oNFEEExI4ynGfiHps60qGG0QWqi0NoyEQEjpqVax3rSbQZE3ak',
+    Access_token_key: '49712490-HqWXlnrUSN7PDlc0akp1teAFx8LxvarXuUU1yztxL',
+    Access_token_secret: '0hSP0vpMzwy6aBMSrtBvOSN7rcjbKiyRM38m45X96DIKk',
     bearer_token: 'AAAAAAAAAAAAAAAAAAAAAJMNNQEAAAAA46Ip5l4MvLUrfg7vBnlPvBT6OYw%3DPXcBbC1DFiHG22emW1W3flblYIUQeWYCDSGiWvYPIZhrPGc2U9'
   });
 
 twitter.get('/', (req, res) => {
-    const params = {screen_name: 'nodeJS'};
+    const params = {screen_name: 'NodeJS'};
+    console.log(params);
 
     client.get(`statuses/user_timeline`, params)
     .then(timeline => {
@@ -20,27 +24,117 @@ twitter.get('/', (req, res) => {
     })
 })
 
-twitter.get('/:name', (req, res) => {
-    // const params = {screen_name: `${req.params.name}`};
-    const params = {screen_name: `${req.params}`};
-    // console.log(req.params.name);
-    // const user = userArray.find(u => u.id == parseInt(req.params.id));
+//gets a users 7 most recent tweets based on user searched input for specific user
+let token = 'AAAAAAAAAAAAAAAAAAAAAJMNNQEAAAAA46Ip5l4MvLUrfg7vBnlPvBT6OYw%3DPXcBbC1DFiHG22emW1W3flblYIUQeWYCDSGiWvYPIZhrPGc2U9';
 
-    client.get(`statuses/user_timeline`, params)
-    .then(timeline => {
-        res.send(timeline);
+twitter.get('/:username', (req, res) => {
+async function getRequest() {
+    const endpointUrl = "https://api.twitter.com/2/tweets/search/recent";
+    const params = {
+        'query': `from:${req.params.username} -is:retweet`,
+        'tweet.fields': 'author_id'
+    }
+
+    const res = await needle('get', endpointUrl, params, {
+        headers: {
+            "User-Agent": "v2RecentSearchJS",
+            "authorization": `Bearer ${token}`
+        }
     })
-    .catch(error => {
-        res.send(error);
-    })
+
+    if (res.body) {
+        return res.body;
+    } else {
+        throw new Error('Unsuccessful request');
+    }
+}
+   (async () => {
+    try {
+        // Make request
+        const response = await getRequest();
+        res.send(response);
+
+    } catch (e) {
+        console.log(e);
+        process.exit(-1);
+    }
+    process.exit();
+})();
 })
+
+const endpointURL = "https://api.twitter.com/2/users/by?usernames="
+
+async function getRequest(req, r) {
+    const params = {
+        usernames: `${req.params.usernames}`, // Edit usernames to look up
+        "user.fields": "created_at,description", // Edit optional query parameters here
+        "expansions": "pinned_tweet_id"
+    }
+
+    const res = await needle('get', endpointURL, params, {
+        headers: {
+            "User-Agent": "v2UserLookupJS",
+            "authorization": `Bearer ${token}`
+        }
+    })
+
+    if (res.body) {
+        return r.send(res.body);
+    } else {
+        throw new Error('Unsuccessful request')
+    }
+}
+
+//search for user. 
+twitter.get('/userbase/:usernames', (req, res) => {
+    
+    getRequest(req, res);
+
+    })
+
+
+//searches for tweets based on hashtags
+twitter.get('/tweets/:hashtag', (req, res) => {
+
+    const endpointURL = "https://api.twitter.com/2/users/by?usernames="
+    async function getRequest() {
+        console.log(req.params.usernames);
+        const params = {
+            // 'query': `#dogs -is:retweet` start here 
+        }
+    
+        const res = await needle('get', endpointURL, params, {
+            headers: {
+                "User-Agent": "v2UserLookupJS",
+                "authorization": `Bearer ${token}`
+            }
+        })
+    
+        if (res.body) {
+            return res.body;
+        } else {
+            throw new Error('Unsuccessful request')
+        }
+    }
+    
+    (async () => {
+    
+        try {
+            // Make request
+            const response = await getRequest();
+            console.dir(response, {
+                depth: null,
+                colors: true
+            });
+
+            res.send(response);
+    
+        } catch (e) {
+            console.log(e);
+            process.exit(-1);
+        }
+        process.exit();
+    })();
+    })
   
-// client.get('statuses/user_timeline', params, function(error, tweets, response) {
-//   if (!error) {
-//     // console.log(tweets);
-//     response.status(200).send(tweets);
-//   }
-// });
- 
-
 module.exports = twitter;
