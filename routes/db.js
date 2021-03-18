78562448884a545d50f2da1ef019a3500fb200ce
elissa <elappenga@gmail.com>
2021-03-17 21:53:49 -0400

    backend complete

--- routes/db.js
+++ routes/db.js
@@ -1,17 +1,145 @@
-const { Pool } = require('pg');
+const express = require('express');
+// const { Pool } = require('pg');
+const db = express.Router();
+
 try {
   // When not running via Heroku, this will load the .env file.
   require('dotenv').config();
 } catch (e) {
   // When running with Heroku, dotenv doesn't need to be available.
 }
-const connectionString = process.env.DATABASE_URL;
-const pool = new Pool({
-  connectionString: connectionString,
-  ssl: connectionString.includes('localhost') ? false : { rejectUnauthorized: false }
+// const connectionString = process.env.DATABASE_URL;
+// const pool = new Pool({
+//   connectionString: connectionString,
+//   ssl: connectionString.includes('localhost') ? false : { rejectUnauthorized: false }
+// });
+
+const pool = require('../connection');
+
+function getTags(req, res) {
+  const tag_list = pool.query(
+    `select tgroups.group_name AS group_name,
+    tags.id AS tag_id,
+    tags.tag_name AS tag
+    from tags
+    inner join tgroups on tgroups.id = tags.tag_group_id
+    order by tgroups.group_name`);
+  res.json(tag_list.rows);
+  res.send(tag_list.rows);
+}
+
+db.get('/', (req, res) => {
+  console.log("hello");
+  getTags(req, res);
 });
 
-module.exports = pool;
+//DB endpoints//
+
+db.get('/', (req, res) => {
+  res.send({data: 'hello'});
+})
+
+//Get all handles
+db.get('/handles', async (req, res) => {
+  try {
+      const account = await pool.query(
+          `select * from handles
+          order by handles.handle`);
+
+          res.json(account.rows);
+  } catch (error) {
+      console.error(err.message);
+  }
+  });
+
+//Get tags for handle//
+db.get('/handles/:handle', async (req, res) => {
+  const { handle } = req.params;
+  try {
+      const account = await pool.query(
+          `select h.handle AS handle,
+          tagmap.tag_id AS tag_id,
+          tags.tag_name AS tag_name
+          from handles h 
+          inner join tagmap on tagmap.handle_id = h.id 
+          inner join tags on tagmap.tag_id = tags.id 
+          where h.handle iLIKE $1
+          order by h.handle`, [handle]);
+
+          res.json(account.rows);
+  } catch (error) {
+      console.error(err.message);
+  }
+  });
+
+  //Get all handles with Tag value//
+  db.get('/tags/:tag', async (req, res) => {
+  const { tag } = req.params;
+  try {
+      const tag_name = await pool.query(
+          `select tags.tag_name AS tag_name, 
+          h.handle AS handle 
+          from handles h 
+          inner join tagmap on tagmap.handle_id = h.id
+          inner join tags on tagmap.tag_id = tags.id
+          where tags.tag_name=$1
+          order by tags.tag_name`, [tag]);
+      res.json(tag_name.rows);
+  } catch (error) {
+      console.error(err.message);
+  }
+  });
+
+  //Get current list of tag groups and their tags//
+  db.get('/tag-groups', async (req, res) => {
+  try {
+      const tag_list = await pool.query(
+          `select tgroups.group_name AS group_name,
+          tags.id AS tag_id,
+          tags.tag_name AS tag
+          from tags
+          inner join tgroups on tgroups.id = tags.tag_group_id
+          order by tgroups.group_name`);
+      res.json(tag_list.rows);
+      res.send(tag_list.rows);
+  } catch (error) {
+      console.error(err.message);        
+  }
+  });
+
+//Add new handle to db//
+
+db.post("/add-handle", async (req, res) => {
+  try {
+      const { handle } = req.body;
+      const newHandle = await pool.query(
+          "INSERT INTO handles (handle) VALUES ($1) RETURNING *",
+          [handle]
+      );
+
+      res.json(newHandle.rows);
+  } catch (err) {
+      console.error(err.message);
+  }
+});
+
+db.post("/add-tagmap", async (req, res) => {
+  try {
+      const { handle_id } = req.body;
+      const { tag_id } = req.body;
+      const newMapping = await pool.query(
+          "INSERT INTO tagmap (handle_id, tag_id) VALUES ($1, $2) RETURNING *",
+          [handle_id, tag_id]
+      );
+
+      res.json(newMapping.rows);
+  } catch (err) {
+      console.error(err.message);
+  }
+});
+
+module.exports = db;
+
 
 
 // const Pool = require('pg').Pool;
--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(express.urlencoded({extended: false}))
 app.use(express.json())
 
 app.use('/twitter', twitter);
+app.use('/db', pool);
 
 app.listen(port, () => console.log(`listening on port ${port}`));
 
@@ -28,81 +29,6 @@ app.listen(port, () => console.log(`listening on port ${port}`));
 app.get("/", (req, res) => {
     res.send({data:'data'});
 })
-app.use('/twitter', twitter);
 
-//DB endpoints//
 
-app.get('/', (req, res) => {
-    res.send({data: 'hello'});
-})
-
-//Get tags for handle//
-app.get('/handles/:handle', async (req, res) => {
-    const { handle } = req.params;
-    try {
-        const account = await pool.query(
-            `select h.handle AS handle,
-            array_agg(tagmap.tag_id) AS tag_ids,
-            array_agg(tags.tag_name) AS tags
-            from handles h 
-            inner join tagmap on tagmap.handle_id = h.id 
-            inner join tags on tagmap.tag_id = tags.id 
-            where h.handle iLIKE $1
-            group by h.handle`, [handle]);
-
-            res.json(account.rows);
-    } catch (error) {
-        console.error(err.message);
-    }
-    });
-
-    //Get all handles with Tag value//
-    app.get('/tags/:tag', async (req, res) => {
-    const { tag } = req.params;
-    try {
-        const tag_name = await pool.query(
-            `select tags.tag_name AS tag_name, 
-            array_agg(h.handle) AS handles 
-            from handles h 
-            inner join tagmap on tagmap.handle_id = h.id
-            inner join tags on tagmap.tag_id = tags.id
-            where tags.tag_name=$1
-            group by tags.tag_name`, [tag]);
-        res.json(tag_name.rows);
-    } catch (error) {
-        console.error(err.message);
-    }
-    });
-
-    //Get current list of tag groups and their tags//
-    app.get('/tag-groups', async (req, res) => {
-    try {
-        const tag_list = await pool.query(
-            `select tgroups.group_name AS group_name,
-            array_agg(tags.id) AS tag_ids,
-            array_agg(tags.tag_name) AS tags
-            from tags
-            inner join tgroups on tgroups.id = tags.tag_group_id
-            group by tgroups.group_name`);
-        res.json(tag_list.rows);
-    } catch (error) {
-        console.error(err.message);        
-    }
-    });
-
-//Add new handle to db//
-
-app.post("/add-handle", async (req, res) => {
-    try {
-        const { handle } = req.body;
-        const newHandle = await pool.query(
-            "INSERT INTO handles (handle) VALUES ($1) RETURNING *",
-            [handle]
-        );
-
-        res.json(newHandle.rows);
-    } catch (err) {
-        console.error(err.message);
-    }
-})
 
